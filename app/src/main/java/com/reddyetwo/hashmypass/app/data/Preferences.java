ec858ec81bb34ee5d9477a48be74c3e249293f59
Gustavo García Pascual <gustavo.mondron@gmail.com>
2014-06-06 19:51:28 +0200

    Added class for specifying Preferences constants

---
+++ app/src/main/java/com/reddyetwo/hashmypass/app/data/Preferences.java
@@ -0,0 +1,7 @@
+package com.reddyetwo.hashmypass.app.data;
+
+public class Preferences {
+
+    public static final String PREFS_NAME = "MyPreferences";
+    public static final String PREFS_KEY_LAST_PROFILE = "LastProfile";
+}
