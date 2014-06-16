b9ab478c74e78d93d5ad58d2061ce739cd4a57b0
Gustavo García Pascual <gustavo.mondron@gmail.com>
2014-06-16 19:38:07 +0200

    Cleaned PasswordHasher code

--- app/src/main/java/com/reddyetwo/hashmypass/app/hash/PasswordHasher.java
+++ app/src/main/java/com/reddyetwo/hashmypass/app/hash/PasswordHasher.java
@@ -7,8 +7,6 @@ import com.reddyetwo.hashmypass.app.data.PasswordType;
 import java.security.InvalidKeyException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
@@ -25,25 +23,14 @@ public class PasswordHasher {
      */
     private static final String DIGEST_SHA1 = "SHA-1";
 
-    /**
-     * A pattern that matches numerical digits
-     */
-    private static final Pattern NUM_PATTERN = Pattern.compile("[^0-9]");
-
-    /**
-     * Matches all special characters
-     */
-    private static final Pattern SPECIAL_PATTERN =
-            Pattern.compile("[^a-z0-9]", Pattern.CASE_INSENSITIVE);
 
     private static String _hashPassword(String tag, String key, int length,
                                         PasswordType type) {
 
-        Mac hmac = null;
+        Mac hmac;
         try {
             hmac = Mac.getInstance(HMAC_SHA1);
         } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
             return null;
         }
 
@@ -68,16 +55,19 @@ public class PasswordHasher {
         if (type == PasswordType.NUMERIC) {
             hash = convertToDigits(hash, sum, length);
         } else {
-           /* We force digits, punctuation characters and mixed case in order to
-        provide compatibility with the Chrome extension */
+            /* We force digits, punctuation characters and mixed case in
+            order to provide compatibility with the Chrome extension */
+            // Force digits
             hash = injectCharacter(hash, 0, 4, sum, length, '0', 10);
             if (type == PasswordType.ALPHANUMERIC_AND_SPECIAL_CHARS) {
+                // Force special chars
                 hash = injectCharacter(hash, 1, 4, sum, length, '!', 15);
             }
+            // Force mixed case
             hash = injectCharacter(hash, 2, 4, sum, length, 'A', 26);
             hash = injectCharacter(hash, 3, 4, sum, length, 'a', 26);
 
-            /* Remove special chars if needed */
+            // Remove special chars if needed
             if (type == PasswordType.ALPHANUMERIC) {
                 hash = removeSpecialCharacters(hash, sum, length);
             }
@@ -97,87 +87,107 @@ public class PasswordHasher {
                     PasswordType.ALPHANUMERIC_AND_SPECIAL_CHARS);
         }
 
+        // Then, hash the result with the master key
         return _hashPassword(tag, masterKey, length, passwordType);
     }
 
+    /**
+     * Calculates the digest of the master key
+     *
+     * @param key the key
+     * @return the digest of the key
+     */
     public static String calculateKeyDigest(String key) {
-        MessageDigest messageDigest = null;
+        MessageDigest messageDigest;
         try {
             messageDigest = MessageDigest.getInstance(DIGEST_SHA1);
         } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
+            return null;
         }
         return Base64.encodeToString(messageDigest.digest(key.getBytes()),
                 Base64.NO_PADDING | Base64.NO_WRAP).substring(0, 2);
     }
 
     /**
-     * Converts the input string to a digits-only representation.
+     * Convert input string to digits-only
+     *
+     * @param input  input string
+     * @param seed   seed for pseudo-randomizing the position and injected character
+     * @param length length of head of string that will eventually survive truncation
+     * @return the digits-only string
      */
-    private static String convertToDigits(String sInput, int seed, int lenOut) {
-        StringBuilder s = new StringBuilder(lenOut);
-        int i = 0;
-        while (i < lenOut) {
-            Matcher m = NUM_PATTERN.matcher(sInput.substring(i));
-            if (!m.find()) break;
-            int matchPos = m.start();
-            if (matchPos > 0) {
-                s.append(sInput.substring(i, i + matchPos));
+    private static String convertToDigits(String input, int seed, int length) {
+        char[] inputChars = input.toCharArray();
+        int pivot = 0; // Pivot for next char-to-digit conversion
+        for (int i = 0; i < length; i++) {
+            if (!Character.isDigit(inputChars[i])) {
+                inputChars[i] = (char) ((seed + inputChars[pivot]) % 10 + '0');
+                pivot = i + 1;
             }
-            s.append((char) ((seed + sInput.charAt(i)) % 10 + 48));
-            i += (matchPos + 1);
         }
-        if (i < sInput.length()) {
-            s.append(sInput.substring(i));
-        }
-        return s.toString();
+
+        return String.valueOf(inputChars);
     }
 
     /**
-     * Replace special characters by digits and numbers.
+     * Replace special chars with alphanumeric chars
+     *
+     * @param input  input string
+     * @param seed   seed for pseudo-randomizing the position and injected
+     *               character
+     * @param length length of head of string that will eventually survive
+     *               truncation
+     * @return the string with special chars replaced
      */
-    private static String removeSpecialCharacters(String sInput, int seed,
-                                                  int lenOut) {
-        StringBuilder s = new StringBuilder(lenOut);
-        int i = 0;
-        while (i < lenOut) {
-            Matcher m = SPECIAL_PATTERN.matcher(sInput.substring(i));
-            if (!m.find()) break;
-            int matchPos = m.start();
-            if (matchPos > 0) s.append(sInput.substring(i, i + matchPos));
-            s.append((char) ((seed + i) % 26 + 65));
-            i += (matchPos + 1);
+    private static String removeSpecialCharacters(String input, int seed,
+                                                  int length) {
+        char[] inputChars = input.toCharArray();
+        int pivot = 0;
+        for (int i = 0; i < length; i++) {
+            if (!Character.isLetterOrDigit(inputChars[i])) {
+                inputChars[i] = (char) ((seed + pivot) % 26 + 'A');
+                pivot = i + 1;
+            }
         }
-        if (i < sInput.length()) s.append(sInput.substring(i));
-        return s.toString();
+
+        return String.valueOf(inputChars);
     }
 
     /**
-     * Inject a character chosen from a range of character codes into a block at the front of a
-     * string if one of those characters is not already present.
+     * Inject a character chosen from a range of character codes into a block
+     * at the front of a string if one of those characters is not already
+     * present
+     *
+     * @param input    input string
+     * @param offset   offset for position of injected character
+     * @param reserved number of offsets reserved for special chars
+     * @param seed     seed for pseudo-randomizing the position and
+     *                 injected character
+     * @param length   length of head of string that will eventually
+     *                 survive truncation
+     * @param cStart   character code for first valid injected character
+     * @param cNum     number of valid character codes starting from cStart
+     * @return the string with the injected chars
      */
-    private static String injectCharacter(String sInput, int offset,
-                                          int reserved, int seed, int lenOut,
+    private static String injectCharacter(String input, int offset,
+                                          int reserved, int seed, int length,
                                           char cStart, int cNum) {
-        int pos0 = seed % lenOut;
-        int pos = (pos0 + offset) % lenOut;
-        // Check if a qualified character is already present
+        int pos0 = seed % length;
+        int pos = (pos0 + offset) % length;
+        // Check if a qualified character is already present.
         // Write the loop so that the reserved block is ignored.
-        for (int i = 0; i < lenOut - reserved; i++) {
-            int i2 = (pos0 + reserved + i) % lenOut;
-            char c = sInput.charAt(i2);
+        for (int i = 0; i < length - reserved; i++) {
+            int i2 = (pos0 + reserved + i) % length;
+            char c = input.charAt(i2);
             if (c >= cStart && c < cStart + cNum)
-                return sInput; // Already present - nothing to do
+                return input; // Already present - nothing to do
         }
 
-        StringBuilder result = new StringBuilder();
-        if (pos > 0) {
-            result.append(sInput.substring(0, pos));
-        }
-        result.append((char) (((seed + sInput.charAt(pos)) % cNum) + cStart));
-        if (pos + 1 < sInput.length()) {
-            result.append(sInput.substring(pos + 1, sInput.length()));
-        }
-        return result.toString();
+        String head = pos > 0 ? input.substring(0, pos) : "";
+        char inject = (char) (((seed + input.charAt(pos)) % cNum) + cStart);
+        String tail =
+                (pos + 1 < input.length()) ? input.substring(pos + 1) : "";
+
+        return head + inject + tail;
     }
 }
