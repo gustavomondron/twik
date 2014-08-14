86c953fd9aa20d5b14b76c8d223db06e483e97e8
Gustavo García Pascual <gustavo.mondron@gmail.com>
2014-08-14 17:15:49 +0200

    Remove unused imports

--- app/src/main/java/com/reddyetwo/hashmypass/app/MainActivity.java
+++ app/src/main/java/com/reddyetwo/hashmypass/app/MainActivity.java
@@ -21,7 +21,6 @@ package com.reddyetwo.hashmypass.app;
 
 import android.app.ActionBar;
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.hardware.SensorManager;
@@ -29,14 +28,12 @@ import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.OrientationEventListener;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.TextView;
 
 import com.reddyetwo.hashmypass.app.data.Preferences;
