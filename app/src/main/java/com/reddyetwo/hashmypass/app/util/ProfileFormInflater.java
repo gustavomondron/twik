9714cc25acd67d19150c7656602372759e9170b3
Gustavo G. Pascual <gustavo.mondron@gmail.com>
2015-01-25 11:04:17 +0100

    Hide ProfileFormInflater public constructor

--- app/src/main/java/com/reddyetwo/hashmypass/app/util/ProfileFormInflater.java
+++ app/src/main/java/com/reddyetwo/hashmypass/app/util/ProfileFormInflater.java
@@ -29,26 +29,26 @@ import com.reddyetwo.hashmypass.app.data.PasswordType;
 
 public class ProfileFormInflater {
 
-    public static void populatePasswordLengthSpinner(Context context,
-                                                Spinner spinner,
-                                               int passwordLength) {
+    private ProfileFormInflater() {
+
+    }
+    
+    public static void populatePasswordLengthSpinner(Context context, Spinner spinner,
+                                                     int passwordLength) {
         ArrayAdapter<String> passwordLengthAdapter =
-                new ArrayAdapter<>(context,
-                        android.R.layout.simple_spinner_item,
+                new ArrayAdapter<>(context, android.R.layout.simple_spinner_item,
                         new String[]{String.valueOf(passwordLength)});
-        passwordLengthAdapter.setDropDownViewResource(
-                android.R.layout.simple_spinner_dropdown_item);
+        passwordLengthAdapter
+                .setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(passwordLengthAdapter);
     }
 
-    public static void populatePasswordTypeSpinner(Context context,
-                                                Spinner spinner,
-                                             PasswordType passwordType) {
+    public static void populatePasswordTypeSpinner(Context context, Spinner spinner,
+                                                   PasswordType passwordType) {
         ArrayAdapter<CharSequence> adapter = ArrayAdapter
                 .createFromResource(context, R.array.password_types_array,
                         android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(
-                android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinner.setSelection(passwordType.ordinal());
     }
