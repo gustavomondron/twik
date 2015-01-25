d6e4ed5e3642e9d2d3bf1f6044f34c8ccbba5c6e
Gustavo G. Pascual <gustavo.mondron@gmail.com>
2015-01-25 10:41:26 +0100

    Simplify switch statement

--- app/src/main/java/com/reddyetwo/hashmypass/app/MovementTouchListener.java
+++ app/src/main/java/com/reddyetwo/hashmypass/app/MovementTouchListener.java
@@ -29,7 +29,8 @@ class MovementTouchListener implements View.OnTouchListener {
     private static final int STATE_PRESSING = 1;
     private static final int STATE_RELEASED = 2;
 
-    private static final int MOVEMENT_THRESHOLD_DP = 15; // 15 dp ~ 3 mm
+    // Threshold ~= 3 mm
+    private static final int MOVEMENT_THRESHOLD_DP = 15;
 
     private int mState = STATE_RELEASED;
     private final OnPressedListener mOnPressedListener;
@@ -49,37 +50,28 @@ class MovementTouchListener implements View.OnTouchListener {
 
     @Override
     public boolean onTouch(View v, MotionEvent event) {
-
-        switch (mState) {
-            case STATE_RELEASED:
-                switch (event.getAction()) {
-                    case MotionEvent.ACTION_DOWN:
-                        mState = STATE_PRESSING;
-                        mX = event.getX();
-                        mY = event.getY();
-                        break;
-                    default:
-                }
-                break;
-            case STATE_PRESSING:
-                switch (event.getAction()) {
-                    case MotionEvent.ACTION_UP:
+        if (mState == STATE_RELEASED && event.getAction() == MotionEvent.ACTION_DOWN) {
+            mState = STATE_PRESSING;
+            mX = event.getX();
+            mY = event.getY();
+        } else if (mState == STATE_PRESSING) {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_UP:
+                    mState = STATE_RELEASED;
+                    mOnPressedListener.onPressed();
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    if (Math.abs(event.getX() - mX) >
+                            mMovementThresholdPx ||
+                            Math.abs(event.getY() - mY) >
+                                    mMovementThresholdPx) {
                         mState = STATE_RELEASED;
-                        mOnPressedListener.onPressed();
-                        break;
-                    case MotionEvent.ACTION_MOVE:
-                        if (Math.abs(event.getX() - mX) >
-                                mMovementThresholdPx ||
-                                Math.abs(event.getY() - mY) >
-                                        mMovementThresholdPx) {
-                            mState = STATE_RELEASED;
-                        }
-                        break;
-                    default:
-                        mState = STATE_RELEASED;
-                }
+                    }
+                    break;
+                default:
+                    mState = STATE_RELEASED;
+            }
         }
-
         return true;
     }
 
