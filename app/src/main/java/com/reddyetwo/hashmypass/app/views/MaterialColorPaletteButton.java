b3eddcbeb9d7905d6a4af043841d4f09ac1ad399
Gustavo G. Pascual <gustavo.mondron@gmail.com>
2015-01-25 10:36:16 +0100

    Merge if statements

--- app/src/main/java/com/reddyetwo/hashmypass/app/views/MaterialColorPaletteButton.java
+++ app/src/main/java/com/reddyetwo/hashmypass/app/views/MaterialColorPaletteButton.java
@@ -126,19 +126,17 @@ public class MaterialColorPaletteButton extends ImageButton {
     }
 
     private void setMarginsWithoutShadow() {
-        if (!mMarginsSet) {
-            if (getLayoutParams() instanceof ViewGroup.MarginLayoutParams) {
-                ViewGroup.MarginLayoutParams layoutParams =
-                        (ViewGroup.MarginLayoutParams) getLayoutParams();
-                int leftMargin = layoutParams.leftMargin - mShadowSize;
-                int topMargin = layoutParams.topMargin - mShadowSize;
-                int rightMargin = layoutParams.rightMargin - mShadowSize;
-                int bottomMargin = layoutParams.bottomMargin - mShadowSize;
-                layoutParams.setMargins(leftMargin, topMargin, rightMargin, bottomMargin);
+        if (!mMarginsSet && getLayoutParams() instanceof ViewGroup.MarginLayoutParams) {
+            ViewGroup.MarginLayoutParams layoutParams =
+                    (ViewGroup.MarginLayoutParams) getLayoutParams();
+            int leftMargin = layoutParams.leftMargin - mShadowSize;
+            int topMargin = layoutParams.topMargin - mShadowSize;
+            int rightMargin = layoutParams.rightMargin - mShadowSize;
+            int bottomMargin = layoutParams.bottomMargin - mShadowSize;
+            layoutParams.setMargins(leftMargin, topMargin, rightMargin, bottomMargin);
 
-                requestLayout();
-                mMarginsSet = true;
-            }
+            requestLayout();
+            mMarginsSet = true;
         }
     }
 
