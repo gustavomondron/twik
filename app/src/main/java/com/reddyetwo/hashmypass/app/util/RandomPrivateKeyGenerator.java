ec99ed44400632c6770ccc62ca2ef98a9424a58d
Gustavo G. Pascual <gustavo.mondron@gmail.com>
2015-01-25 11:06:43 +0100

    Hide RandomPrivateKeyGenerator public constructor

--- app/src/main/java/com/reddyetwo/hashmypass/app/util/RandomPrivateKeyGenerator.java
+++ app/src/main/java/com/reddyetwo/hashmypass/app/util/RandomPrivateKeyGenerator.java
@@ -26,9 +26,12 @@ public class RandomPrivateKeyGenerator {
 
     private static final int[] SUBGROUPS_LENGTH = {8, 4, 4, 4, 12};
     private static final char SUBGROUP_SEPARATOR = '-';
-    private static final String ALLOWED_CHARS =
-            "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private static final String ALLOWED_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
+    private RandomPrivateKeyGenerator() {
+
+    }
+    
     public static String generate() {
         SecureRandom sr = new SecureRandom();
         String key = "";
@@ -36,8 +39,7 @@ public class RandomPrivateKeyGenerator {
 
         for (int i = 0; i < SUBGROUPS_LENGTH.length; i++) {
             for (int j = 0; j < SUBGROUPS_LENGTH[i]; j++) {
-                key = key +
-                        ALLOWED_CHARS.charAt(sr.nextInt(allowedCharsLength));
+                key = key + ALLOWED_CHARS.charAt(sr.nextInt(allowedCharsLength));
             }
             if (i < SUBGROUPS_LENGTH.length - 1) {
                 key += SUBGROUP_SEPARATOR;
