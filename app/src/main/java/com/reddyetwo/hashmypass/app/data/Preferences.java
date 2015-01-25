d18b69642aa53cf329a8bc8166522a369db788c4
Gustavo G. Pascual <gustavo.mondron@gmail.com>
2015-01-25 11:02:53 +0100

    Hide Preferences public constructor

--- app/src/main/java/com/reddyetwo/hashmypass/app/data/Preferences.java
+++ app/src/main/java/com/reddyetwo/hashmypass/app/data/Preferences.java
@@ -31,20 +31,22 @@ public class Preferences {
     public static final String PREFS_KEY_LAST_PROFILE = "LastProfile";
     public static final String PREFS_KEY_TUTORIAL_PAGE = "tutorialPage";
 
+    private Preferences() {
+        
+    }
+
     public static int getRememberMasterKeyMins(Context context) {
         SharedPreferences preferences =
                 context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
-        return Integer.decode(preferences.getString(
-                context.getString(R.string.settings_key_remember_master_key),
-                context.getString(
-                        R.string.settings_default_remember_master_key)));
+        return Integer.decode(preferences
+                .getString(context.getString(R.string.settings_key_remember_master_key),
+                        context.getString(R.string.settings_default_remember_master_key)));
     }
 
     public static boolean getCopyToClipboard(Context context) {
         SharedPreferences preferences =
                 context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
-        return preferences.getBoolean(context.getString(R.string
-                .settings_key_copy_to_clipboard), context.getResources()
-                .getBoolean(R.bool.settings_default_copy_to_clipboard));
+        return preferences.getBoolean(context.getString(R.string.settings_key_copy_to_clipboard),
+                context.getResources().getBoolean(R.bool.settings_default_copy_to_clipboard));
     }
 }
