2853d5ca6843e8abcaaf5744b558ce1769da43e4
Gustavo G. Pascual <gustavo.mondron@gmail.com>
2015-02-08 13:00:35 +0100

    More HelpToastOnLongPressClickListener tests

--- app/src/test/java/com/reddyetwo/hashmypass/app/util/HelpToastOnLongPressClickListenerTest.java
+++ app/src/test/java/com/reddyetwo/hashmypass/app/util/HelpToastOnLongPressClickListenerTest.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.View;
 import android.widget.Toast;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -11,6 +12,7 @@ import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
@@ -31,20 +33,48 @@ public class HelpToastOnLongPressClickListenerTest {
     @Mock
     private Toast toast;
 
-    @Test
-    public void shouldShowToastOnLongClick() {
-        // Given
+    @Before
+    public void setUp() throws Exception {
         mockStatic(Toast.class);
-        when(view.getContentDescription()).thenReturn(text);
-        when(text.length()).thenReturn(1);
         when(view.getContext()).thenReturn(context);
         when(Toast.makeText(context, text, Toast.LENGTH_SHORT)).thenReturn(toast);
         PowerMockito.doNothing().when(toast).show();
+    }
+
+    @Test
+    public void shouldShowToastOnLongClick() {
+        // Given
+        when(view.getContentDescription()).thenReturn(text);
+        when(text.length()).thenReturn(1);
 
         // When
         new HelpToastOnLongPressClickListener().onLongClick(view);
 
         // Then
         verify(toast).show();
+    }
+
+    @Test
+    public void shouldNotShowToastOnLongClickWhenContentIsNull() {
+        // Given
+
+        // When
+        new HelpToastOnLongPressClickListener().onLongClick(view);
+
+        // Then
+        verify(toast, never()).show();
+    }
+
+    @Test
+    public void shouldNotShowToastOnLongClickWhenContentIsEmpty() {
+        // Given
+        when(view.getContentDescription()).thenReturn(text);
+        when(text.length()).thenReturn(0);
+
+        // When
+        new HelpToastOnLongPressClickListener().onLongClick(view);
+
+        // Then
+        verify(toast, never()).show();
     }
 }
