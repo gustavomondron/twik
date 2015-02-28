76c28cbaafa275f7af5137b16ff4a50d36e35d4f
Gustavo G. Pascual <gustavo.mondron@gmail.com>
2015-02-28 14:20:29 +0100

    Remove duplicated String literals

--- app/src/main/java/com/reddyetwo/hashmypass/app/data/DataOpenHelper.java
+++ app/src/main/java/com/reddyetwo/hashmypass/app/data/DataOpenHelper.java
@@ -34,6 +34,20 @@ class DataOpenHelper extends SQLiteOpenHelper {
 
     public static final String COLUMN_ID = "_id";
 
+    /* SQL commands */
+    private static final String SQL_CREATE_TABLE = "CREATE TABLE ";
+    private static final String SQL_ALTER_TABLE = "ALTER TABLE ";
+    private static final String SQL_INTEGER = " INTEGER";
+    private static final String SQL_PRIMARY_KEY = " PRIMARY KEY";
+    private static final String SQL_TEXT = " TEXT";
+    private static final String SQL_NOT_NULL = " NOT NULL";
+    private static final String SQL_DEFAULT = " DEFAULT 0";
+    private static final String SQL_FIELD_SEPARATOR = ", ";
+    private static final String SQL_FOREIGN_KEY = "FOREIGN KEY ";
+    private static final String SQL_REFERENCES = " REFERENCES ";
+    private static final String SQL_UNIQUE = "UNIQUE ";
+    private static final String SQL_ADD_COLUMN = " ADD COLUMN ";
+
     /* Table "profiles" */
     public static final String PROFILES_TABLE_NAME = "profiles";
     public static final String COLUMN_PROFILES_NAME = "name";
@@ -42,15 +56,13 @@ class DataOpenHelper extends SQLiteOpenHelper {
     public static final String COLUMN_PROFILES_PASSWORD_TYPE = "password_type";
     public static final String COLUMN_PROFILES_COLOR_INDEX = "color_index";
     private static final String PROFILES_TABLE_CREATE =
-            "CREATE TABLE " + PROFILES_TABLE_NAME + " (" +
-                    COLUMN_ID + " INTEGER PRIMARY KEY, " +
-                    COLUMN_PROFILES_NAME + " TEXT, " +
-                    COLUMN_PROFILES_PRIVATE_KEY + " TEXT, " +
-                    COLUMN_PROFILES_PASSWORD_LENGTH + " INTEGER, " +
-                    COLUMN_PROFILES_PASSWORD_TYPE + " INTEGER, " +
-                    COLUMN_PROFILES_COLOR_INDEX + " INTEGER NOT NULL DEFAULT " +
-                    "0" +
-                    ");";
+            SQL_CREATE_TABLE + PROFILES_TABLE_NAME + " (" +
+                    COLUMN_ID + SQL_INTEGER + SQL_PRIMARY_KEY + SQL_FIELD_SEPARATOR +
+                    COLUMN_PROFILES_NAME + SQL_TEXT + SQL_FIELD_SEPARATOR +
+                    COLUMN_PROFILES_PRIVATE_KEY + SQL_TEXT + SQL_FIELD_SEPARATOR +
+                    COLUMN_PROFILES_PASSWORD_LENGTH + SQL_INTEGER + SQL_FIELD_SEPARATOR +
+                    COLUMN_PROFILES_PASSWORD_TYPE + SQL_INTEGER + SQL_FIELD_SEPARATOR +
+                    COLUMN_PROFILES_COLOR_INDEX + SQL_INTEGER + SQL_NOT_NULL + SQL_DEFAULT + ");";
 
     /* Table "tags" */
     public static final String TAGS_TABLE_NAME = "tags";
@@ -60,36 +72,34 @@ class DataOpenHelper extends SQLiteOpenHelper {
     public static final String COLUMN_TAGS_HASH_COUNTER = "hash_counter";
     public static final String COLUMN_TAGS_PASSWORD_LENGTH = "password_length";
     public static final String COLUMN_TAGS_PASSWORD_TYPE = "password_type";
-    private static final String TAGS_TABLE_CREATE = "CREATE TABLE " + TAGS_TABLE_NAME + " (" +
-            COLUMN_ID + " INTEGER PRIMARY KEY, " +
-            COLUMN_TAGS_NAME + " TEXT, " +
-            COLUMN_TAGS_PROFILE_ID + " INTEGER, " +
-            COLUMN_TAGS_HASH_COUNTER + " INTEGER NOT NULL DEFAULT 0, " +
-            COLUMN_TAGS_SITE + " TEXT, " +
-            COLUMN_TAGS_PASSWORD_LENGTH + " INTEGER, " +
-            COLUMN_TAGS_PASSWORD_TYPE + " INTEGER, " +
-            "FOREIGN KEY(" + COLUMN_TAGS_PROFILE_ID + ") REFERENCES " +
-            PROFILES_TABLE_NAME + "(id)" +
-            "UNIQUE (" + COLUMN_TAGS_NAME + "," +
-            "" + COLUMN_TAGS_PROFILE_ID + ")," +
-            "UNIQUE (" + COLUMN_TAGS_PROFILE_ID + "," +
-            "" + COLUMN_TAGS_SITE + ")" +
-            ");";
-    private static final String TAGS_TABLE_ADD_HASH_COUNTER_COLUMN = "" +
-            "ALTER TABLE " + TAGS_TABLE_NAME + " ADD COLUMN " +
-            COLUMN_TAGS_HASH_COUNTER + " INTEGER NOT NULL DEFAULT 0;";
+    private static final String TAGS_TABLE_CREATE = SQL_CREATE_TABLE + TAGS_TABLE_NAME + " (" +
+            COLUMN_ID + SQL_INTEGER + SQL_PRIMARY_KEY + SQL_FIELD_SEPARATOR +
+            COLUMN_TAGS_NAME + SQL_TEXT + SQL_FIELD_SEPARATOR +
+            COLUMN_TAGS_PROFILE_ID + SQL_INTEGER + SQL_FIELD_SEPARATOR +
+            COLUMN_TAGS_HASH_COUNTER + SQL_INTEGER + SQL_NOT_NULL + SQL_DEFAULT +
+            SQL_FIELD_SEPARATOR + COLUMN_TAGS_SITE + SQL_TEXT + SQL_FIELD_SEPARATOR +
+            COLUMN_TAGS_PASSWORD_LENGTH + SQL_INTEGER + SQL_FIELD_SEPARATOR +
+            COLUMN_TAGS_PASSWORD_TYPE + SQL_INTEGER + SQL_FIELD_SEPARATOR +
+            SQL_FOREIGN_KEY + "(" + COLUMN_TAGS_PROFILE_ID + ")" + SQL_REFERENCES +
+            PROFILES_TABLE_NAME + "(id)" + SQL_UNIQUE + "(" + COLUMN_TAGS_NAME +
+            SQL_FIELD_SEPARATOR + COLUMN_TAGS_PROFILE_ID + ")," +
+            SQL_UNIQUE + "(" + COLUMN_TAGS_PROFILE_ID + SQL_FIELD_SEPARATOR +
+            COLUMN_TAGS_SITE + ")" + ");";
+    private static final String TAGS_TABLE_ADD_HASH_COUNTER_COLUMN =
+            SQL_ALTER_TABLE + TAGS_TABLE_NAME + SQL_ADD_COLUMN + COLUMN_TAGS_HASH_COUNTER +
+                    SQL_INTEGER + SQL_NOT_NULL + SQL_DEFAULT;
 
     private static final String PROFILES_TABLE_ADD_COLOR_INDEX_COLUMN = "" +
-            "ALTER TABLE " + PROFILES_TABLE_NAME + " ADD COLUMN " +
-            COLUMN_PROFILES_COLOR_INDEX + " INTEGER NOT NULL DEFAULT 0;";
+            SQL_ALTER_TABLE + PROFILES_TABLE_NAME + SQL_ADD_COLUMN +
+            COLUMN_PROFILES_COLOR_INDEX + SQL_INTEGER + SQL_NOT_NULL + SQL_DEFAULT;
 
     /* Table "favicons" */
     public static final String FAVICONS_TABLE_NAME = "favicons";
     public static final String COLUMN_FAVICONS_SITE = "site";
-    private static final String FAVICONS_TABLE_CREATE = "CREATE TABLE " +
-            FAVICONS_TABLE_NAME + " (" + COLUMN_ID + " INTEGER PRIMARY KEY, " +
-            COLUMN_FAVICONS_SITE + " TEXT, " +
-            "UNIQUE (" + COLUMN_FAVICONS_SITE + "));";
+    private static final String FAVICONS_TABLE_CREATE = SQL_CREATE_TABLE +
+            FAVICONS_TABLE_NAME + " (" + COLUMN_ID + SQL_INTEGER + SQL_PRIMARY_KEY +
+            SQL_FIELD_SEPARATOR + COLUMN_FAVICONS_SITE + SQL_TEXT + SQL_FIELD_SEPARATOR +
+            SQL_UNIQUE + "(" + COLUMN_FAVICONS_SITE + "));";
 
     public DataOpenHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION_CURRENT);
