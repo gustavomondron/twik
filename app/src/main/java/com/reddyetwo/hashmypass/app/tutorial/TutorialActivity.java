dbf1776fe9535852d41f4387a9a542166c307662
Gustavo G. Pascual <gustavo.mondron@gmail.com>
2015-02-28 13:09:32 +0100

    Remove unused imports

--- app/src/main/java/com/reddyetwo/hashmypass/app/tutorial/TutorialActivity.java
+++ app/src/main/java/com/reddyetwo/hashmypass/app/tutorial/TutorialActivity.java
@@ -20,7 +20,6 @@
 package com.reddyetwo.hashmypass.app.tutorial;
 
 import android.animation.ArgbEvaluator;
-import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.support.annotation.ColorRes;
 import android.support.v4.app.Fragment;
@@ -28,8 +27,6 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.text.Layout;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
